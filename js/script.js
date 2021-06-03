48ca20dfd8e136c8af9b720623673f1930ba9a75
kuzaevrr <kuzaevrr@mail.ru>
2021-06-03 14:44:07 +0500

    init commit

---
+++ js/script.js
@@ -0,0 +1,51 @@
+"use strict";
+
+
+let incr = 10, 
+    decr = 10;
+
+// incr++;
+// decr--;
+
+console.log(++incr);
+console.log(--decr);
+
+
+console.log(2*4 === 'abc');
+
+// const category = 'toys';
+
+// console.log(`https://someurl.com/${category}/5`);
+
+// const user = "Ivan";
+
+// alert(`Привет ${user}`);
+
+// alert('Hello');
+
+
+// const result = confirm("Are you here?");
+// console.log(result);
+
+// const answer = +prompt("Вам есть 18?", "18");
+// console.log(answer + 5);
+
+// const answers = [];
+
+// answers[0] = prompt('Как Вас зовут?','');
+// answers[1] = prompt('Какая у Вас фамилия?','');
+// answers[2] = prompt('Сколько Вам лет?','');
+
+// console.log(typeof(answers));
+
+// const obj = {
+//     name: "Jonh",
+//     age: 25,
+//     isMarried: false
+// };
+
+// // console.log(obj.name);
+// console.log(obj["name"]);
+
+// let arr = ['plum.png','orange.jpg', 6, 'apple.bmp', {}, []];
+// console.log(arr[1]);

90f2375f55267d47b5112aedea836dd19c35f951
Ramil <kuzaevrr@mail.ru>
2021-06-05 13:11:54 +0500

    Pr. ch1

--- js/script.js
+++ js/script.js
@@ -1,51 +1,21 @@
 "use strict";
 
 
-let incr = 10, 
-    decr = 10;
+const numberOfFilms = +prompt('Cколько фильмов вы уже посмотрели?', '');
 
-// incr++;
-// decr--;
+const personalMovieDB ={
+    count: numberOfFilms,
+    movies: {},
+    actors: {},
+    genres: [],
+    private: false
+};
+const   a = prompt('Один из последних просомтренных фильмов?',''),
+        b = prompt('На сколько оцените его?',''),
+        c = prompt('Один из последних просомтренных фильмов?',''),
+        d = prompt('На сколько оцените его?','');
 
-console.log(++incr);
-console.log(--decr);
+        personalMovieDB.movies[a] = b;
+        personalMovieDB.movies[c] = d;
 
-
-console.log(2*4 === 'abc');
-
-// const category = 'toys';
-
-// console.log(`https://someurl.com/${category}/5`);
-
-// const user = "Ivan";
-
-// alert(`Привет ${user}`);
-
-// alert('Hello');
-
-
-// const result = confirm("Are you here?");
-// console.log(result);
-
-// const answer = +prompt("Вам есть 18?", "18");
-// console.log(answer + 5);
-
-// const answers = [];
-
-// answers[0] = prompt('Как Вас зовут?','');
-// answers[1] = prompt('Какая у Вас фамилия?','');
-// answers[2] = prompt('Сколько Вам лет?','');
-
-// console.log(typeof(answers));
-
-// const obj = {
-//     name: "Jonh",
-//     age: 25,
-//     isMarried: false
-// };
-
-// // console.log(obj.name);
-// console.log(obj["name"]);
-
-// let arr = ['plum.png','orange.jpg', 6, 'apple.bmp', {}, []];
-// console.log(arr[1]);
+console.log(personalMovieDB);
